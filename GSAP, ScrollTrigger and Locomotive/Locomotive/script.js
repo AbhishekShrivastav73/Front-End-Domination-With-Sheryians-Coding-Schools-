26e9687328083008b5c8155cd1e922978a4034bb
Abhishek Shrivastav <147899930+AbhishekShrivastav73@users.noreply.github.com>
2024-02-23 23:30:30 +0530

    Add files via upload

---
+++ GSAP, ScrollTrigger and Locomotive/Locomotive/script.js
@@ -0,0 +1,4 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
---
+++ GSAP, ScrollTrigger and Locomotive/Locomotive/script2.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#wrapper"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#wrapper" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#wrapper", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#wrapper").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#wrapper'),
+    smooth: true
+});
+
+
+gsap.from('.page1 .box',{
+    opacity : 0,
+    rotate : 360,
+    duration : 1,
+    delay :1
+})
+gsap.from('.page2 .box',{
+    opacity : 0,
+    rotate : 360,
+    duration : 1,
+    scrollTrigger : {
+        trigger : '.page2 .box',
+        scroller : '#wrapper',
+        markers : true,
+        start : "top 50%"
+    }
+})
